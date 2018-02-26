c4c2f9a158a14113659ecd950a30ec74b76cc105
MALLE-PC\Malin <malin.thelin12@gmail.com>
2018-02-26 10:03:24 +0100

    humans moving back and forth while not abducted

--- human.h
+++ human.h
@@ -18,26 +18,25 @@ class HumanBehaviourComponent : public Component
 	bool abducted = false;
 	bool goingRight = true;
 	float startTime;
-	float changeDirectionTime = 5.0f;
+	float changeDirectionTime;
 public:
 	virtual void Create(AvancezLib* system, GameObject * go, std::set<GameObject*> * game_objects)
 	{
 		Component::Create(system, go, game_objects);
 		startTime = system->getElapsedTime();
+		changeDirectionTime = rand() % 5 + 1;
 	}
 
 	virtual void Update(float dt) 
 	{
-		/*
 		if (!abducted) { //move side to side 
-			float movement = goingRight ? HUMAN_SPEED * dt : HUMAN_SPEED * dt;
+			float movement = goingRight ? HUMAN_SPEED * dt : -HUMAN_SPEED * dt;
 			go->horizontalPosition += movement;
 
-			if ((system->getElapsedTime() - startTime) >= changeDirectionTime) {
+			if ((system->getElapsedTime() - startTime) > changeDirectionTime) {
 				goingRight = (goingRight == false); //flip bool
 				startTime = system->getElapsedTime(); //reset time
 			}
 		}
-		*/
 	}
 };
